c4d52426995a65551d234d216873440ccc50e45b
bendera <szoftverhiba@gmail.com>
2024-10-01 00:45:41 +0200

    Remove unused code

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,12 @@
 // @ts-check
 
 import { VscodeDemo } from "./demo.js";
-import { DEMO_TAG_NAME, TOOLBAR_TAG_NAME } from "./shared.js";
 import { VscodeDevToolbar } from "./toolbar.js";
 import { VscodeThemeSelector } from "./theme-selector.js";
 import { VscodeViewContainerSelector } from "./view-container-selector.js";
 
-customElements.define(TOOLBAR_TAG_NAME, VscodeDevToolbar);
-customElements.define(DEMO_TAG_NAME, VscodeDemo);
+customElements.define("vscode-dev-toolbar", VscodeDevToolbar);
+customElements.define("vscode-demo", VscodeDemo);
 customElements.define("vscode-theme-selector", VscodeThemeSelector);
 customElements.define(
   "vscode-view-container-selector",
