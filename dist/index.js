ddee3f7f143a6fadc8107f41da15ba7623508ca0
bendera <szoftverhiba@gmail.com>
2024-10-01 21:53:54 +0200

    Order imports

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,16 @@
 import { VscodeDemo } from "./demo.js";
 import { VscodeDevToolbar } from "./toolbar.js";
 import { VscodeThemeSelector } from "./theme-selector.js";
-import { VscodeViewContainerSelector } from "./view-container-selector.js";
-import { VscodeToggleUnderline } from "./toggle-underline.js";
 import { VscodeToggleMotion } from "./toggle-motion.js";
+import { VscodeToggleUnderline } from "./toggle-underline.js";
+import { VscodeViewContainerSelector } from "./view-container-selector.js";
 
-customElements.define("vscode-dev-toolbar", VscodeDevToolbar);
 customElements.define("vscode-demo", VscodeDemo);
+customElements.define("vscode-dev-toolbar", VscodeDevToolbar);
 customElements.define("vscode-theme-selector", VscodeThemeSelector);
+customElements.define("vscode-toggle-motion", VscodeToggleMotion);
+customElements.define("vscode-toggle-underline", VscodeToggleUnderline);
 customElements.define(
   "vscode-view-container-selector",
   VscodeViewContainerSelector
 );
-customElements.define("vscode-toggle-underline", VscodeToggleUnderline);
-customElements.define("vscode-toggle-motion", VscodeToggleMotion);
