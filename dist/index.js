b4f0d9cb246ed9014ffdf18e111b498e9231cec3
bendera <szoftverhiba@gmail.com>
2024-10-01 21:50:30 +0200

    Register toggle components

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,8 @@ import { VscodeDemo } from "./demo.js";
 import { VscodeDevToolbar } from "./toolbar.js";
 import { VscodeThemeSelector } from "./theme-selector.js";
 import { VscodeViewContainerSelector } from "./view-container-selector.js";
+import { VscodeToggleUnderline } from "./toggle-underline.js";
+import { VscodeToggleMotion } from "./toggle-motion.js";
 
 customElements.define("vscode-dev-toolbar", VscodeDevToolbar);
 customElements.define("vscode-demo", VscodeDemo);
@@ -12,3 +14,5 @@ customElements.define(
   "vscode-view-container-selector",
   VscodeViewContainerSelector
 );
+customElements.define("vscode-toggle-underline", VscodeToggleUnderline);
+customElements.define("vscode-toggle-motion", VscodeToggleMotion);
